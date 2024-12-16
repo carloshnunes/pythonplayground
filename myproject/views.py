a6cb6316b2ddc993de61848732ceb60a58c0576d
carloshnunes <110049855+carloshnunes@users.noreply.github.com>
2024-12-16 18:33:21 -0300

    wop: estilizando o projeto

--- myproject/views.py
+++ myproject/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
   tasks = ["Estudar Django", "Criar um projeto", "Aprender templates"]
   return render(request, 'home.html', {'tasks': tasks})
+
+def about(request):
+    return render(request, 'about.html')
